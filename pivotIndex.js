fa5ed9e79de24aa528e7a93ad32bfa76d1209e90
kaplandavid992 <kaplandavid992@gmail.com>
2022-10-24 22:25:59 +0300

    pivot index

--- pivotIndex.js
+++ pivotIndex.js
@@ -1,52 +1,28 @@
 
+
 var pivotIndex = function (nums) {
-  let i = 0;
-  let j = nums.length - 1;
-  let rightSideSum = 0;
-  let leftSideSum = 0;
-  let addRight = true;
-  let addLeft = true;
+ 
+  function sumOfArrEls(arr){
+  return arr.reduce(
+    (totalValue, currentValue) => {
+      return totalValue + currentValue
+  }, 0);
+ }
 
-  const addRightFig = () => {
-    i++;
-    addLeft = true;
-    addRight = false;
-  };
-
-  const addLeftFig = () => {
-    j--;
-    addRight = true;
-    addLeft = false;
-  };
-
-  const sumsAreBothNeg = () => {
-    return leftSideSum < 0 && rightSideSum < 0;
-  };
-
-  while (j >= i + 1) {
-    addRight ? (rightSideSum += nums[j]) : null;
-    addLeft ? (leftSideSum += nums[i]) : null;
-
-    if (leftSideSum > rightSideSum || (leftSideSum < 0 && rightSideSum >= 0)) {
-    } else if (
-      leftSideSum < rightSideSum ||
-      (rightSideSum < 0 && leftSideSum >= 0)
-    ) {
-      sumsAreBothNeg() ? addLeftFig() : addRightFig();
-    } else {
-      i++;
-      j--;
-      addLeft = true;
-      addRight = true;
-    }
+  for(let i=0;i<=nums.length-1 ;i++){
+    let leftSideArr=[0];
+    if(i>0){
+    leftSideArr = nums.slice(0, i)};
+  
+    let rightSideArr = nums.slice(i+1);
+   
+    if ( sumOfArrEls(rightSideArr) === sumOfArrEls(leftSideArr) || i===0 && sumOfArrEls(rightSideArr)===0)
+    
+    return i; 
   }
+  return -1;
+}
 
-  if (leftSideSum === rightSideSum && i === j) {
-    return i;
-  } else if (leftSideSum === 0 || rightSideSum === 0) {
-    return 0;
-  } else {
-    return -1;
-  }
-};
+
+
 
